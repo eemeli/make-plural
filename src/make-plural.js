efa478629027e43c64c242ec61d4a793fe57de41
Eemeli Aro <eemeli@gmail.com>
2016-10-14 01:19:15 +0300

    Fix remaining lint issues

--- src/make-plural.js
+++ src/make-plural.js
@@ -89,23 +89,30 @@ class Tests {
   }
 
   testCond(n, type, expResult, fn) {
-    try { var r = (fn || this.obj.fn)(n, (type === 'ordinal')) }
-    catch (e) { r = e.toString() }
-    if (r !== expResult) { throw new Error(
-      'Locale ' + JSON.stringify(this.obj.lc) + type
-      + ' rule self-test failed for v = ' + JSON.stringify(n)
-      + ' (was ' + JSON.stringify(r) + ', expected ' + JSON.stringify(expResult) + ')'
-    ) }
+    try {
+      var r = (fn || this.obj.fn)(n, (type === 'ordinal'))
+    } catch (e) {
+      r = e.toString()
+    }
+    if (r !== expResult) {
+      throw new Error(
+        'Locale ' + JSON.stringify(this.obj.lc) + type +
+        ' rule self-test failed for v = ' + JSON.stringify(n) +
+        ' (was ' + JSON.stringify(r) + ', expected ' + JSON.stringify(expResult) + ')'
+      )
+    }
     return true
   }
 
   testCat(type, cat, fn) {
     const data = this[type][cat]
-    if (!data.values) data.values = data.src.join(' ')
-      .replace(/^[ ,]+|[ ,…]+$/g, '')
-      .replace(/(0\.[0-9])~(1\.[1-9])/g, '$1 1.0 $2')
-      .split(/[ ,~…]+/)
-    data.values.forEach( n => {
+    if (!data.values) {
+      data.values = data.src.join(' ')
+        .replace(/^[ ,]+|[ ,…]+$/g, '')
+        .replace(/(0\.[0-9])~(1\.[1-9])/g, '$1 1.0 $2')
+        .split(/[ ,~…]+/)
+    }
+    data.values.forEach(n => {
       this.testCond(n, type, cat, fn)
       if (!/\.0+$/.test(n)) this.testCond(Number(n), type, cat, fn)
     })
