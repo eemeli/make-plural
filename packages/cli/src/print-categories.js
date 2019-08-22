3421285e571df8e5c750d1a3458607565d75feb4
Eemeli Aro <eemeli@gmail.com>
2019-08-22 12:39:04 +0300

    feat(cli): Automate common-categories detection

--- packages/cli/src/print-categories.js
+++ packages/cli/src/print-categories.js
@@ -1,8 +1,8 @@
 import { identifier } from 'safe-identifier'
-import { cardinalCategories, combinedCategories } from './common-categories'
 import getCompiler from './get-compiler'
 import printUMD from './print-umd'
 
+const MAX_CATEGORIES_REPEAT = 5
 const NAMES = { zero: 'z', one: 'o', two: 't', few: 'f', many: 'm', other: 'x' }
 
 function stringifyCategories({ cardinal, ordinal }) {
@@ -15,42 +15,47 @@ function stringifyCategories({ cardinal, ordinal }) {
 
 export default function printCategoriesModule(args) {
   const MakePlural = getCompiler(args)
-  const { locale, ordinals, umd } = args
+  const { locale, umd } = args
   const locales =
     locale.length === 0 ? Object.keys(MakePlural.rules.cardinal) : locale.sort()
-  const commonCategories = ordinals ? combinedCategories : cardinalCategories
 
-  const usedCommonCategories = {}
-  const categories = locales.map(lc => {
+  const localesByCat = {}
+  for (const lc of locales) {
     const mpc = new MakePlural(lc)
     mpc.compile()
     mpc.test()
     const cat = stringifyCategories(mpc.categories)
-    const i = commonCategories.indexOf(cat)
-    if (i === -1) {
-      return [lc, cat]
-    } else {
-      usedCommonCategories[i] = true
-      return [lc, `_${i}`]
-    }
-  })
+    const id = identifier(lc)
+    const prev = localesByCat[cat]
+    if (prev) prev.push(id)
+    else localesByCat[cat] = [id]
+  }
 
   const varType = umd ? 'var' : 'const'
   const varNames = Object.keys(NAMES).map(name => `${NAMES[name]} = "${name}"`)
   let str = `${varType} ${varNames.join(', ')};\n`
-  for (let i = 0; i < commonCategories.length; ++i) {
-    if (usedCommonCategories[i])
-      str += `${varType} _${i} = ${commonCategories[i]};\n`
+  let commonId = 'a'
+  const categories = []
+  for (const [cat, locales] of Object.entries(localesByCat)) {
+    if (locales.length > MAX_CATEGORIES_REPEAT && commonId <= 'z') {
+      str += `${varType} ${commonId} = ${cat};\n`
+      for (const lc of locales) categories.push({ lc, cat: commonId })
+      do {
+        commonId = String.fromCharCode(commonId.charCodeAt(0) + 1)
+      } while (Object.values(NAMES).includes(commonId))
+    } else {
+      for (const lc of locales) categories.push({ lc, cat })
+    }
   }
+  categories.sort((a, b) => (a.lc < b.lc ? -1 : 1))
   if (str) str += '\n'
+
   if (umd) {
-    const cm = categories.map(([lc, cat]) => `${identifier(lc)}: ${cat}`)
+    const cm = categories.map(({ lc, cat }) => `${lc}: ${cat}`)
     str += printUMD('pluralCategories', cm.join(',\n')) + '\n'
   } else {
-    for (const [lc, cat] of categories) {
-      const id = identifier(lc)
-      str += `export const ${id} = ${cat};\n`
-    }
+    for (const { lc, cat } of categories)
+      str += `export const ${lc} = ${cat};\n`
   }
   return str
 }
